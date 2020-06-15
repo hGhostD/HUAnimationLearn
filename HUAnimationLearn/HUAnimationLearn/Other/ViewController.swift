11c32f2318b8acf2d226ec7cf798c513b69d4f6c
Hu Jiawen <jw.hu@s2icode.com>
2020-06-15 18:06:52 +0800

    bezier

--- HUAnimationLearn/HUAnimationLearn/Other/ViewController.swift
+++ HUAnimationLearn/HUAnimationLearn/Other/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    let cellID = "tableViewCell"
+    let dataArr = [HUBezierCircleController()]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataArr.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID)
+        
+        cell?.textLabel?.text = String(indexPath.row)
+        return cell!
     }
 
-
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        self.navigationController?.pushViewController(dataArr[indexPath.row], animated: true)
+    }
 }
 
---
+++ HUAnimationLearn/HUAnimationLearn/第二章/HUBezierCircleController.swift
@@ -0,0 +1,30 @@
+//
+//  HUBezierCircleController.swift
+//  HUAnimationLearn
+//
+//  Created by jw.hu on 2020/6/15.
+//  Copyright © 2020 jw.hu. All rights reserved.
+//
+
+import UIKit
+
+class HUBezierCircleController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
